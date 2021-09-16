884da5d9387bf9cb3d6302badfe689589a8aba71
justinaus <justinaus2k@gmail.com>
2021-09-16 14:33:46 +0900

    Add eslint/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'next',
+    'prettier',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
+  ],
+  plugins: [
+    'prettier',
+    'react',
+    'unused-imports',
+    'simple-import-sort',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'prettier/prettier': ['warn'],
+    'unused-imports/no-unused-imports': ['warn'],
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+  },
+};
